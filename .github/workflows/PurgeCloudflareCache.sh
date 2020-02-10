e15fd0254d6d5080235a098ec1def0eebd8a8254
Dean Murphy <deanouk@icloud.com>
2020-02-10 01:59:21 +0000

    Added script to purge Cloudflare Cache

---
+++ .github/workflows/PurgeCloudflareCache.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+if [ -z "${CLOUDFLARE_ZONE}" ]; then
+    echo "Cloudflare zone not found"
+    exit 1
+fi
+
+if [ -z "${CLOUDFLARE_API_TOKEN}" ]; then
+    echo "Cloudflare auth key not found"
+    exit 2
+fi
+
+response=$(curl -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" -H "Content-Type: application/json" --data '{"purge_everything":true}')
+
+echo "Response data: $response"
+
+if [[ $response =~ '"success":true' ]]; then
+    echo "Cloudflare cache cleared successfully"
+    exit 0
+else
+    echo "Cloudflare API call failed"
+    exit 3
+fi
